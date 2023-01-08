b589512d9fe71e42ed2ddba1ea4f89d4893cf19a
ohperes <peresvini2@gmail.com>
2023-01-07 21:29:24 -0300

    feat: set background

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,13 @@ export const GlobalStyle = createGlobalStyle`
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']}
   }
 
+  body {
+    -webkit-font-smoothing: antialiased;
+    background-image: url('../../../src/assets/background.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top center;
+  }
 
   body, input, textarea, button {
     font: 400 1rem Roboto, sans-serif;
