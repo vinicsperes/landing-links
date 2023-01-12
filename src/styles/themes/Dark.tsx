503b727c20b4c06f7eb506a2b6149103aba36f09
ohperes <peresvini2@gmail.com>
2023-01-12 18:47:32 -0300

    feat: create Light and Dark themes

---
+++ src/styles/themes/Dark.tsx
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+  textColor: 'white',
+  bgLightPath: '../../../src/assets/backgrounds/bg-dark.png',
+  bgLightMobilePath: '../../../src/assets/backgrounds/bg-dark-mobile.png',
+  strokeColor: 'rgba(255, 255, 255, 0.5)',
+  surfaceColor: 'rgba(255, 255, 255, 0.1)',
+  surfaceColorHover: 'rgba(255, 255, 255, 0.5)',
+  highlightColor: 'rgba(255, 255, 255, 0.2)',
+  switchIconPath: 'src/assets/icons/sun.svg',
+}
---
+++ src/styles/themes/Light.tsx
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+  textColor: '#0c0c0c',
+  bgLightPath: '../../../src/assets/backgrounds/bg-light.png',
+  bgLightMobilePath: '../../../src/assets/backgrounds/bg-light-mobile.png',
+  strokeColor: 'rgba(0, 0, 0, 0.5)',
+  surfaceColor: 'rgba(0, 0, 0, 0.05)',
+  surfaceColorHover: 'rgba(0, 0, 0, 0.02)',
+  highlightColor: 'rgba(0, 0, 0, 0.1)',
+  switchIconPath: 'src/assets/icons/moonStars.svg',
+}
